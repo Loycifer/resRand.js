ca49312fc8479db4f0c68f204ef023637e5b721e
Loy Clements <loy.clements@gmail.com>
2014-10-28 18:21:02 +0100

    Length and license
    resRand.length
    Added read-only .length property which returns the length of the current
    array.
    
    Added license headers

--- resRand.js
+++ resRand.js
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2014 Loy Clements
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 (function(window) {
     var resRand = {
 	sourceList: [],
@@ -207,8 +224,12 @@
 	    {
 		console.log(this.workingList[i]);
 	    }
-	}
+	},
+	length: {}
 
     };
+    Object.defineProperty(resRand, "length", {
+	get: function(){return resRand.workingList.length;}
+    });
     window["resRand"] = resRand;
 })(window);
